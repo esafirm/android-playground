eec84984b678d22d209e8475319e29b706372a74
Esa Firman <esafirm21@gmail.com>
2016-11-01 21:00:17 +0700

    Change MainActivity implementation

--- app/src/main/java/com/esafirm/androidplayground/main/MainActivity.java
+++ app/src/main/java/com/esafirm/androidplayground/main/MainActivity.java
@@ -1,42 +1,44 @@
 package com.esafirm.androidplayground.main;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
-import com.esafirm.androidplayground.R;
+import com.esafirm.androidplayground.common.MenuFactory;
+import com.esafirm.androidplayground.common.OnNavigatePage;
 import com.esafirm.androidplayground.dagger.example.DaggerSampleAct;
+import com.esafirm.androidplayground.rxjava2.RxJavaSampleAct;
 
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(
+                MenuFactory.create(this,
+                        Arrays.asList(
+                                "Dagger Example",
+                                "RxJava2"
+                        ),
+                        new OnNavigatePage() {
+                            @Override
+                            public void navigate(int index) {
+                                navigateToPage(index);
+                            }
+                        })
+        );
+    }
 
-        MainListAdapter adapter = new MainListAdapter(this);
-        adapter.setData(Arrays.asList(
-                "Dagger Example"
-        ));
-
-        ListView listView = (ListView) findViewById(R.id.listview);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0:
-                        DaggerSampleAct.start(context);
-                        break;
-                }
-            }
-        });
+    private void navigateToPage(int index) {
+        switch (index) {
+            case 0:
+                DaggerSampleAct.start(this);
+                break;
+            case 1:
+                RxJavaSampleAct.start(this);
+                break;
+        }
     }
 }
