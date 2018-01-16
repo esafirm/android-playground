55bd2247edd5a88d336ce8047207946de2c1c559
Esa Firman <esafirm21@gmail.com>
2018-01-16 10:13:25 +0700

    Fix appbar behavoir

--- app/src/main/java/com/esafirm/androidplayground/ui/nestedscroll/NestedScrollingController.kt
+++ app/src/main/java/com/esafirm/androidplayground/ui/nestedscroll/NestedScrollingController.kt
@@ -1,8 +1,9 @@
 package com.esafirm.androidplayground.ui.nestedscroll
 
 import android.os.Bundle
+import android.support.design.widget.AppBarLayout
+import android.support.design.widget.CoordinatorLayout
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.LinearSnapHelper
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import butterknife.BindView
@@ -12,6 +13,7 @@ import com.esafirm.conductorextra.butterknife.BinderController
 class NestedScrollingController : BinderController() {
 
     @BindView(R.id.rv) lateinit var recycler: RecyclerView
+    @BindView(R.id.appbar) lateinit var appBar: AppBarLayout
 
     override fun getLayoutResId(): Int = R.layout.controller_nested_scroll_test
 
@@ -19,6 +21,8 @@ class NestedScrollingController : BinderController() {
         val adapter = TestAdapter(bindingResult.context)
         recycler.layoutManager = LinearLayoutManager(bindingResult.context, LinearLayoutManager.HORIZONTAL, false)
         recycler.adapter = adapter
-        LinearSnapHelper().attachToRecyclerView(recycler)
+
+        appBar.layoutParams.let { it as CoordinatorLayout.LayoutParams }
+                .behavior = FixAppBarLayoutBehavior()
     }
 }
